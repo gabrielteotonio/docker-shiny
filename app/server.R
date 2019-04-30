62808cb9507fae5541ff766f36b24800e892aa98
gabrielteotonio <gabriel.teotonio@ubee.in>
2019-04-30 14:56:51 -0300

    .pdf presentation added

--- app/server.R
+++
@@ -1,9 +0,0 @@
-function(input, output) {
-  
-  output$imgPlot <- renderPlot({
-    img <- compression(PCA$x, PCA$eigenvectors, input$n_pc)
-    par(mfrow=c(1,2))
-    image(img, col=gray(0:100/100), axes=FALSE)
-    image(lena, col=gray(0:100/100), axes=FALSE)
-  })
-}
