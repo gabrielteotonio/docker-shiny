35ce5f1bd47b3304bce99ffa9baaa8e0a85232a4
gabrielteotonio <gabriel.teotonio@ubee.in>
2019-04-30 14:46:32 -0300

    Shiny app created and readme with installation steps

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# docker-shiny
+# Using Shiny application with Docker
+
+
+
+## 1. Installing Docker
+
+```
+sudo apt-get install docker.io
+sudo dnf installl docker
+```
+
+## 2. Running R inside a container
+
+```
+docker run  --rm --name=rgui -it r-base 
+```
+  
+## 3. Running Rstudio inside a container
+
+## 4. Shiny app and Docker
---
+++ app/ui.R
@@ -0,0 +1,20 @@
+fluidPage(    
+  
+  titlePanel("Image Compression - PCA"),
+  
+  sidebarLayout(      
+    
+    sidebarPanel(
+      sliderInput(inputId = "n_pc", 
+                  label = "Number of PC to be used in the compression:",
+                  value = 3,
+                  min = 1,
+                  max = 64)
+    ),
+    
+    mainPanel(
+      plotOutput("imgPlot")  
+    )
+    
+  )
+)
